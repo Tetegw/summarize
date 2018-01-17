39f9ab56747eb9a9326d5e025353530b015052f7
Tetegw <1151832942@qq.com>
2018-01-17 17:48:37 +0800

    [added] test

---
+++ 【学习】svg/test.js
@@ -0,0 +1,1 @@
+// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%253A%252F%252Fbank-static-stg.pingan.com.cn%252Fcreditcard%252Fcardservice%252FwxServer%252FwxServer.html%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
