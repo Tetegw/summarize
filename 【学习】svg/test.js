1181b2fece4825386e98e29b9fe391d44db2135a
Tetegw <1151832942@qq.com>
2018-01-24 10:49:22 +0800

    [test] test

--- 【学习】svg/test.js
+++ 【学习】svg/test.js
@@ -1,7 +1,39 @@
-// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%253A%252F%252Fbank-static-stg.pingan.com.cn%252Fcreditcard%252Fcardservice%252FwxServer%252FwxServer.html%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+## 本地
+// 解绑
+http://localhost:8080/wxServer/wxServer.html?fn=offBind&openid=oqwCLwlfn7C0X9Sngvg2UEiWyBgU&nickname=%E7%BB%8F%E8%BF%87%E5%8D%97%E5%9F%8E&picurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2F57C8xOwKqMCBE93DCLhM3loiauqgk1F6QBzB22abb90J5ZxSCuO8rWbia0hFERIK94Jc4tgYK0xrFsGTerSWcjE8gmmaLWTU3U%2F132&subscribe=1&isBind=1&signature=859919df6096ad02748e17808e8efbb7a2dc68b6&nonce=45356280&timestamp=1516617947152#/
+
+// 绑定
+http://localhost:8080/wxServer/wxServer.html?fn=onBind&openid=oqwCLwlfn7C0X9Sngvg2UEiWyBgU&nickname=%E7%BB%8F%E8%BF%87%E5%8D%97%E5%9F%8E&picurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2F57C8xOwKqMCBE93DCLhM3loiauqgk1F6QBzB22abb90J5ZxSCuO8rWbia0hFERIK94Jc4tgYK0xrFsGTerSWcjE8gmmaLWTU3U%2F132&subscribe=1&isBind=1&signature=859919df6096ad02748e17808e8efbb7a2dc68b6&nonce=45356280&timestamp=1516617947152#/
 
 
-https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffn%3DonBind%26fromModule%3Dhttps%253A%252F%252Fwww.baidu.com%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+// 功能
+http://localhost:8080/wxServer/wxServer.html?openid=oqwCLwlfn7C0X9Sngvg2UEiWyBgU&nickname=%E7%BB%8F%E8%BF%87%E5%8D%97%E5%9F%8E&picurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2F57C8xOwKqMCBE93DCLhM3loiauqgk1F6QBzB22abb90J5ZxSCuO8rWbia0hFERIK94Jc4tgYK0xrFsGTerSWcjE8gmmaLWTU3U%2F132&subscribe=1&isBind=1&signature=859919df6096ad02748e17808e8efbb7a2dc68b6&nonce=45356280&timestamp=1516617947152&from=https%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fpaces-ccms-core%2Fcreditcard%2FbillDetail%2FbillDetail.html%23%2FbridgeView
 
-https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffn%3DonBind%26from%3Dhttps%253A%252F%252Fbank-static-stg.pingan.com.cn%252Fpaces-ccms-core%252Fcreditcard%252FbillDetail%252FbillDetail.html%2523%252FbridgeView%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+
+## 远程转换
+// 解绑
+https://bank-static-stg.pingan.com.cn/creditcard/cardservice/wxServer/wxServer.html?fn=offBind&openid=oqwCLwlfn7C0X9Sngvg2UEiWyBgU&nickname=%E7%BB%8F%E8%BF%87%E5%8D%97%E5%9F%8E&picurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2F57C8xOwKqMCBE93DCLhM3loiauqgk1F6QBzB22abb90J5ZxSCuO8rWbia0hFERIK94Jc4tgYK0xrFsGTerSWcjE8gmmaLWTU3U%2F132&subscribe=1&isBind=1&signature=859919df6096ad02748e17808e8efbb7a2dc68b6&nonce=45356280&timestamp=1516617947152#/
+
+// 绑定
+https://bank-static-stg.pingan.com.cn/creditcard/cardservice/wxServer/wxServer.html?fn=onBind&openid=oqwCLwlfn7C0X9Sngvg2UEiWyBgU&nickname=%E7%BB%8F%E8%BF%87%E5%8D%97%E5%9F%8E&picurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2F57C8xOwKqMCBE93DCLhM3loiauqgk1F6QBzB22abb90J5ZxSCuO8rWbia0hFERIK94Jc4tgYK0xrFsGTerSWcjE8gmmaLWTU3U%2F132&subscribe=1&isBind=1&signature=859919df6096ad02748e17808e8efbb7a2dc68b6&nonce=45356280&timestamp=1516617947152#/
+
+
+// 功能
+https://bank-static-stg.pingan.com.cn/creditcard/cardservice/wxServer/wxServer.html?openid=oqwCLwlfn7C0X9Sngvg2UEiWyBgU&nickname=%E7%BB%8F%E8%BF%87%E5%8D%97%E5%9F%8E&picurl=http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2F57C8xOwKqMCBE93DCLhM3loiauqgk1F6QBzB22abb90J5ZxSCuO8rWbia0hFERIK94Jc4tgYK0xrFsGTerSWcjE8gmmaLWTU3U%2F132&subscribe=1&isBind=1&signature=859919df6096ad02748e17808e8efbb7a2dc68b6&nonce=45356280&timestamp=1516617947152&from=https%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fpaces-ccms-core%2Fcreditcard%2FbillDetail%2FbillDetail.html%23%2FbridgeView
+
+
+
+## UCP
+
+// 解绑
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffn%3DoffBind%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+
+// 绑定
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffn%3DonBind%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+
+
+// 功能
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffrom%3Dhttps%253A%252F%252Fbank-static-stg.pingan.com.cn%252Fpaces-ccms-core%252Fcreditcard%252FbillDetail%252FbillDetail.html%2523%252FbridgeView%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+
+
 
