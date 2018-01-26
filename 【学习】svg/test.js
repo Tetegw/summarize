d6688e110f45fdbefaba34d7c4656385d24a38d6
Tetegw <1151832942@qq.com>
2018-01-26 11:08:37 +0800

    [test] test

--- 【学习】svg/test.js
+++ 【学习】svg/test.js
@@ -33,7 +33,13 @@ https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&red
 
 
 // 功能
-https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffrom%3Dhttps%253A%252F%252Fbank-static-stg.pingan.com.cn%252Fpaces-ccms-core%252Fcreditcard%252FbillDetail%252FbillDetail.html%2523%252FbridgeView%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffn%3DonBind%26from%3Dhttps%253A%252F%252Fbank-static-stg.pingan.com.cn%252Fpaces-ccms-core%252Fcreditcard%252FbillDetail%252FbillDetail.html%2523%252FbridgeView%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+
+
+
+// 积分专区
+https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxbace4c5540ab3f6e&redirect_uri=http%3A%2F%2Feim-talk-stg.dmzstg.pingan.com.cn%2Fpaxyk-pir%2Fredirect%3FbackUrl%3Dhttps%3A%2F%2Fbank-static-stg.pingan.com.cn%2Fcreditcard%2Fcardservice%2FwxServer%2FwxServer.html%3Ffn%3DonBind%26from%3Dhttps%253A%252F%252Fb-stg.pingan.com.cn%252Fconsume%252FlifeService%252Fintegral%252Fpages%252Findex.html%26weappNo%3DPAXYK95511_01&response_type=code&scope=snsapi_base&state=sig#wechat_redirect
+
 
 
 
