00b06efad3c4ac0d553b5de98c868b1a4936629a
孙晨(总行零售网金) <EX_WLJR_SUNCHEN@pingan.com.cn>
2018-02-01 09:06:26 +0800

    [add] python one day

---
+++ 【学习】python/hello.py
@@ -0,0 +1,28 @@
+# !/usr/bin/env python
+# hello world 
+print 'hello world!'
+print '''
+haha
+hehe
+hengheng
+'''
+
+
+a = 100
+if a >= 0:
+  print a
+else:
+  print -a
+
+print 2 > 3
+
+print True and True
+print True or False
+print not False
+
+print None
+
+PI = 3.1415926
+PI = '11'
+
+print PI
