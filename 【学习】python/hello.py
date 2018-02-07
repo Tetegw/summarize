5239de3c458a22e1c52561f48a26fbf75653ce74
孙晨(总行零售网金) <EX_WLJR_SUNCHEN@pingan.com.cn>
2018-02-07 17:33:14 +0800

    [added] paython three

--- 【学习】python/hello.py
+++ 【学习】python/hello.py
@@ -39,3 +39,49 @@ else:
   
 
 print range(10)
+
+
+while a <= 10:
+    if a > 10:
+      break;
+    print(a)
+    n = n + 1
+print 'D'
+
+
+print '====='
+def my_abs(x):
+    if x > 0:
+      return x
+    elif x < 0:
+      return -x
+print my_abs(1)
+print my_abs(-3)
+
+
+def add_end(L=[]):
+    L.append('END')
+    return L
+
+print '==add_end==='
+print add_end()
+print add_end()
+  
+  
+def add_end_(K=None):
+    if K is None:
+        K = []
+    K.append('END')
+    return K
+print '==add_end_==='
+print add_end_()
+print add_end_()
+
+
+print '==calc==='
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+print calc(1, 3, 5, 7)
