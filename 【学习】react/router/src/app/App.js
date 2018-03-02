42ec00660eb147b2ce87079d6cb2ff33c27dd2b7
Tetegw <115832942@qq.com>
2018-03-02 15:52:22 +0800

    [added] react

--- 【学习】react/router/src/app/App.js
+++ 【学习】react/router/src/app/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css'
 import Hearder from '../Header';
